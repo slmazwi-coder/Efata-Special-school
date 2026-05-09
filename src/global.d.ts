40039259e1e9598ef8820ff9c8dc669d0e3954ef
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-05-09 13:25:43 +0200

    Create global.d.ts

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module "*.png" { const src: string; export default src; }
+declare module "*.jpg" { const src: string; export default src; }
+declare module "*.jpeg" { const src: string; export default src; }
+declare module "*.webp" { const src: string; export default src; }
+declare module "*.svg" { const src: string; export default src; }
+declare module "*.gif" { const src: string; export default src; }
+
+interface ImportMeta {
+  readonly env: Record<string, string | undefined>;
+}
